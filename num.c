2114bd6b620159892500408d588b830a11ea158b
priyarms <30829747+priyarms@users.noreply.github.com>
2017-09-05 16:35:09 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the number');
+scnaf("%d%d%d",&a,&b,&c);
+if(a>b)
+printf("a is big");
+else if(b>c)
+printf("b is big");
+else
+printf("c is big");
+}
