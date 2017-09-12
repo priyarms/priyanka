ea9179aff5e3d611e218e4ebd3632ebce0ed3d25
priyarms <30829747+priyarms@users.noreply.github.com>
2017-09-12 19:21:06 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,16 +3,16 @@ void main()
 {
 char c;
 scanf("%c",&c);
-if(c==a)
+if(c=='a')
 printf("vowel");
-else if(c==e)
+else if(c=='e')
 printf("vowel");
-else if(c==i)
+else if(c=='i')
 printf("vowel");
-else if(c==o)
+else if(c=='o')
 printf("vowel");
-else if(c==u);
+else if(c=='u')
 printf("vowel");
 else
-printf("constant");
+printf("consonant");
 }
