b647d3ad0e7a27e74145662383b8aee70ba5b693
priyarms <30829747+priyarms@users.noreply.github.com>
2017-09-12 16:52:09 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char c;
+scanf("%c",&c);
+if(c==a)
+printf("vowel");
+else if(c==e)
+printf("vowel");
+else if(c==i)
+printf("vowel");
+else if(c==o)
+printf("vowel");
+else if(c==u);
+printf("vowel");
+else
+printf("constant");
+}
