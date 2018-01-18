16f5bc4b46a4f418a6fb158ba707c3837ddbf1ea
priyarms <30829747+priyarms@users.noreply.github.com>
2018-01-18 16:52:08 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 void main()
 {
-int i,sum=0,n;
-printf("enter the limit");
-scanf("%d",&n);
-for(i=0;i<=n;i++)
-{
-sum=sum+i;
+int a[]={0,1,2,3,4,5,6,7,8,9};
+int sum=0,n,k,i;
+ printf("enter n value");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+    printf("%d",a[i]);
+  printf("enter K value");
+  scanf("%d",&k);
+  for(i=0;i<=k;K++)
+  {
+    sum=sum+a[i];
+  }
+  printf("%d",sum);
 }
-printf("sum of value=",sum);
-}
+  
