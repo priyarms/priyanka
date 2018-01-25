141c522b0d3984df437fdc48f94b87f4c73e4415
priyarms <30829747+priyarms@users.noreply.github.com>
2018-01-25 16:09:10 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,a[20],i,m;
+printf("enter a number");
+scanf("%d",&n);
+printf("enter the numbers");
+for(i=0;i<=n;i++)
+scanf("%d",&a[i]);
+m=a[0];
+for(i=1;i<=n;i++)
+{
+if(a[i]>m)
+m=a[i];
+}
+printf("%d",m);
+}
+
