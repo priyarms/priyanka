dd78c1426c3118b6eb7beb7f0847afac44e91b6a
priyarms <30829747+priyarms@users.noreply.github.com>
2018-01-25 16:11:42 +0530

    Update large.c

--- large.c
+++ large.c
@@ -5,10 +5,10 @@ int n,a[20],i,m;
 printf("enter a number");
 scanf("%d",&n);
 printf("enter the numbers");
-for(i=0;i<=n;i++)
+for(i=0;i<n;i++)
 scanf("%d",&a[i]);
 m=a[0];
-for(i=1;i<=n;i++)
+for(i=1;i<n;i++)
 {
 if(a[i]>m)
 m=a[i];
