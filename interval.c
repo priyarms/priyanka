6ecbe21bb1c0c48bbcfcae01b2a9142eabfab198
priyarms <30829747+priyarms@users.noreply.github.com>
2018-02-06 09:43:41 +0000

    Update interval.c

--- interval.c
+++ interval.c
@@ -2,16 +2,21 @@
 void main()
 {
 int s,e,i,m=0,r;
+int x,n;
 printf("enter a number");
 scanf("%d%d",&s,&e);
 for(i=s;i<=e;i++)
 {
-while(i!=0)
+  x=i;
+  n=i;
+  m=0;
+while(x!=0)
 {
-r=i%10;
+r=x%10;
 m=m+(r*r*r);
-i=i/10;
+x=x/10;
 }
+if(m==n)
 printf("%d",m);
 }
 }
