37bfeaa77667d0e4f760c57ffdea150a79409d53
priyarms <30829747+priyarms@users.noreply.github.com>
2018-02-09 09:32:40 +0000

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int i,n,a[50],m,c;
+  printf("enter a number");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("yes");
+  }
+  else
+  printf("no");
+  }
