1e476effd009a4a90c8d031294aeed95024dc1c4
priyarms <30829747+priyarms@users.noreply.github.com>
2018-02-13 08:32:21 +0000

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+  int n;
+  printf("enter a number");
+  scanf("%d",&n);
+  switch(n)
+  {
+    case 1:
+    printf("one");
+    break;
+    case 2:
+    printf("two");
+    break;
+    case 3:
+    printf("three");
+    break;
+    case 4:
+    printf("four");
+    break;
+    case 5:
+    printf("five");
+    break;
+    case 6:
+    printf("six");
+    break;
+    case 7:
+    printf("seven");
+    break;
+    case 8:
+    printf("eight");
+    break;
+    case 9:
+    printf("nine");
+    break;
+    case 10:
+    printf("ten");
+    break;
+    default:
+    printf("exit");
+  }
+}
