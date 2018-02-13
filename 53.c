f868d704cdb897fdaecbaa5a4b4ae2672cf71df5
priyarms <30829747+priyarms@users.noreply.github.com>
2018-02-13 08:40:54 +0000

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  int n,r,sum=0;
+  printf("enter a number");
+  scanf("%d",&n);
+  while(n!=0)
+  {
+    r=n%10;
+    sum=sum+r;
+    n=n/10;
+  }
+  printf("%d",sum);
+}
