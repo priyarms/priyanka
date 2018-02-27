e2050466e5fcce69eac1d70e6813ffaaebee4c48
priyarms <30829747+priyarms@users.noreply.github.com>
2018-02-27 15:50:59 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  int f1=0,f2=1,c,n,i;
+  printf("enter a number");
+  scanf("%d",&n);
+  printf("%d%d",f1,f2);
+  for(i=1;i<n;i++)
+  {
+    c=f1+f2;
+    f1=f2;
+    f2=c;
+    printf("%d",c);
+  }
+  
+}
