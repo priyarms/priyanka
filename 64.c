a11f90e2a6dbb81ef77bbf75b01abf3e8597464c
priyarms <30829747+priyarms@users.noreply.github.com>
2018-03-01 16:02:11 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+  printf("enter numbers");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  if(c%2==0)
+  
+    printf("even");
+    else 
+    printf("odd");
+  
+}
