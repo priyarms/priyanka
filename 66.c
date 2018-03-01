927411e209b69c962d136de70e92a8bdb3d0a876
priyarms <30829747+priyarms@users.noreply.github.com>
2018-03-01 16:33:18 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  int a,i,count=0;
+  printf("enter number");
+  scanf("%d",&a);
+  for(i=1;i<=a;i++)
+  {
+    if(a%i==0)
+    {
+    count++;
+    }
+  }
+  if(count==2)
+  printf("prime number");
+  else
+  printf("not prime number");
+}
