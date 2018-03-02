c41418c5e6dd3f32272c656f71f81fdb5f9c4993
priyarms <30829747+priyarms@users.noreply.github.com>
2018-03-02 15:45:37 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int n;
+  printf("enter a number");
+  scanf("%d",&n);
+  if(n%7==0)
+    printf("multiple of 7");
+    else
+    printf("not multiple of 7");
+  
+}
