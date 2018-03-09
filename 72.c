dcf10230bae195a042c364ad263464bbfcef9fa5
priyarms <30829747+priyarms@users.noreply.github.com>
2018-03-09 16:45:49 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+  char a[30];
+  int i,count=0;
+  printf("enter string");
+  scanf("%s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+{
+count++;
+}
+}
+if(count==0)
+printf("no");
+else
+printf("yes");
+}
