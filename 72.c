ca797f88871e9cf774419c32b00b680c7d380ee1
priyarms <30829747+priyarms@users.noreply.github.com>
2018-03-13 15:59:41 +0530

    Update 72.c

--- 72.c
+++ 72.c
@@ -7,7 +7,7 @@ void main()
   scanf("%s",&a);
 for(i=0;a[i]!='\0';i++)
 {
-if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
 {
 count++;
 }
