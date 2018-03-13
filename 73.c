d468017d6db08f1c5b19f5559199cc8520888b06
priyarms <30829747+priyarms@users.noreply.github.com>
2018-03-13 16:28:41 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,n;
+  printf("number");
+  scanf("%d",&n);
+  printf("enter two numbers");
+  scanf("%d%d",&a,&b);
+  if(n>=a&&n<=b)
+  {
+  printf("yes");
+  }
+  else
+  {
+  printf("no");
+}
+}
