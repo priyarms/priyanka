38a66ca3688c935d31298ac8762222b45cdcb5cd
priyarms <30829747+priyarms@users.noreply.github.com>
2018-03-16 16:08:59 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,i,n;
+  printf("enter no of kabali and ninjas");
+  scanf("%d",&n);
+  printf("enter two numbers");
+  for(i=0;i<n;i++)
+  {
+  scanf("%d %d",&a,&b);
+   printf(" difference %d\n",b-a); 
+  }
+  }
